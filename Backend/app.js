616ad06667dc63c468c7593fe456f344a5aaab03
Pranav Gupta <pranav.19142@davietjal.org>
2023-03-30 16:07:18 +0530

    added build folder in backend

--- Backend/app.js
+++ Backend/app.js
@@ -5,7 +5,7 @@ const completedItems = require("./models/completedItem");
 const PORT = 4000;
 const app = express();
 app.use(express.json());
-
+app.use(express.static('build'));
 app.get("/api/allTodo", async (req, res) => {
   try {
     const result = {
